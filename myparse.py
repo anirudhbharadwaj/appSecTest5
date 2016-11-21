7ae424880f167ce63a68315a965056c0f489f0a6
anirudhbharadwaj <anibharadwaj007@gmail.com>
2016-11-20 19:42:58 -0500

    myparse 2

--- myparse.py
+++ myparse.py
@@ -2,9 +2,16 @@ from git import Repo
 
 repo = Repo('.')
 commits = list(repo.iter_commits('bsl'))
+content1=""
 for i in commits:
+  content2=str(i.tree.blobs[0].data_stream.read())
+  if content1:
+    print content1.replace(content2,"")
+    print "----- end -----\n\n"
+  content1=content2
   print i
   print str(i.hexsha)
   print str(i.committed_date)
   print str(i.message)
-  print "----- end -----\n\n"
+print content1
+print "----- end -----\n\n"
